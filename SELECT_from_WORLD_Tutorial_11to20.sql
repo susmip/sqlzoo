2292ca57bdb6b2bf0cf5dabf007e617ded674220
susmip <98744813+susmip@users.noreply.github.com>
2022-11-11 14:54:39 +0530

    Update SELECT_from_WORLD_Tutorial_11to20.sql

--- SELECT_from_WORLD_Tutorial_11to20.sql
+++ SELECT_from_WORLD_Tutorial_11to20.sql
@@ -9,3 +9,14 @@ SELECT name, capital
  SELECT name, capital
 FROM world
 where LEFT(name,1)=LEFT(capital,1);
+
+
+-- All the vowels
+SELECT name
+   FROM world
+WHERE name like '%a%' and 
+name like '%e%' and 
+name like '%i%' and
+name like '%o%' and 
+name like '%u%' and 
+name not like '% %';
